dfcbe1fdfb4e23ed147de349dfe5557dd82cbde6
mp <mploureno@gmail.com>
2021-01-02 22:14:57 -0300

    define coffee fetcher scope

--- app/models/store.rb
+++ app/models/store.rb
@@ -4,4 +4,11 @@ class Store < ApplicationRecord
   has_many :ratings
   validates :lonlat, :name, :google_place_id, presence: true
   validates :google_place_id, uniqueness: true
+
+  scope :within, -> (longitude, latitude, distance_in_km = 5) {
+    where(%{
+      ST_Distance(lonlat, 'POINT(%f %f)') < %d
+    } % [longitude, latitude, distance_in_km * 1000])
+  }
+    
 end
