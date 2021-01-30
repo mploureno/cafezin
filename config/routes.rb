a00f08715d5a5b1bd377ee58468918a296042c8b
mp <mploureno@gmail.com>
2021-01-30 15:49:49 -0300

    add resourceful routes to google stores

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :stores,  only: [:index, :show], defaults: { format: 'json' }
-      resources :ratings, only: [:create],       defaults: { format: 'json' }
+      resources :stores,  only: %i[index show], defaults: { format: 'json' }
+      resources :ratings, only: %i[create],     defaults: { format: 'json' }
+      resources :google_stores, only: %i[index show]
     end
-  end  
+  end
 end
